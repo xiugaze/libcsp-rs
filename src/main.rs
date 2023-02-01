b428f153e884667e2f0b8711beb72fc7f70e76e0
xiugaze <calebandreano@gmail.com>
2023-02-01 13:44:38 -0600

    memory.x is throwing linker errors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![no_std]
+
 #![no_main]
+#![no_std]
 
-use panic_semihosting as _; // panic handler
-use rtic::app;
+use panic_semihosting as _;
 
-#[app(device = lm3s6965)]
+#[rtic::app(device = stm32f4::stm32f446, peripherals = true)]
 mod app {
-    use cortex_m_semihosting::debug;
+    use cortex_m_semihosting::{debug, hprintln};
 
     #[shared]
     struct Shared {}
@@ -14,9 +14,25 @@ mod app {
     #[local]
     struct Local {}
 
-    #[init]
-    fn init(_: init::Context) -> (Shared, Local, init::Monotonics) {
-        debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
+    #[init(local = [x: u32 = 0])]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Cortex-M peripherals
+        let _core: cortex_m::Peripherals = cx.core;
+
+        // Device specific peripherals
+        let _device: stm32f4::stm32f446::Peripherals = cx.device;
+
+        // Locals in `init` have 'static lifetime
+        let _x: &'static mut u32 = cx.local.x;
+
+        // Access to the critical section token,
+        // to indicate that this is a critical seciton
+        let _cs_token: bare_metal::CriticalSection = cx.cs;
+
+        hprintln!("init");
+
+        //debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
+
         (Shared {}, Local {}, init::Monotonics())
     }
 }
