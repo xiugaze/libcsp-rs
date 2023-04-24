2b4b4f98ea15d182cc5ccb0988eaca489b338157
Caleb Andreano <calebandreano@gmail.com>
2023-04-24 13:21:27 -0500

    listener on port 10

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
 use std::net::{TcpListener, TcpStream};
-use std::thread;
 use std::io::{Read, Write, Error};
 
 pub mod csp;
 
-
-
-fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-    println!("Incoming connection from: {}", stream.peer_addr()?);
-    let mut buf = [0; 512];
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 { return Ok(())};
-        stream.write(&buf[..bytes_read])?;
-    }
-}
-
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8888").expect("Could not bind to address");
-    for stream in listener.incoming() {
-        match stream {
-            Err(e) => { eprintln!("failed: {}", e) }
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
-                });
-            }
+    let mut stream = TcpStream::connect("127.0.0.1:10").unwrap();
+
+    let mut buffer = [0; 1024];
+    loop {
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        if bytes_read == 0 {
+            break;
         }
+    }
+    for (i, b) in buffer.iter().enumerate() {
+        println!("idx: {:?} = {:?}", i, b);
     }
 }
 
