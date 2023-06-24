4248122788a82e03e2eb91e3cba76fe75aa3b590
Caleb Andreano <calebandreano@gmail.com>
2023-06-24 15:31:17 -0500

    Feat: Receiving packets from C version and packing correctly, default CspId

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 pub mod csp;
-use csp::Csp;
+use csp::{Csp, port::CspSocket};
 
 fn main() {
     let mut csp = Csp::default();
-    csp.add_interface("udp 8080 0");
+    // lport (receive on): 8080
+    // rport (send to): 8090
+    csp.add_interface("udp 8080 8090");
+    // conn_less = true
+    let socket = CspSocket::new(false);
+    csp.bind(socket);
     loop {
         csp.route_work();
     }
+}
+
+fn server() {
+    println!("Server Task Started");
+    // Socket (conn_less = false);
+    let socket = CspSocket::new(false);
+    csp.bind(socket);
 }
