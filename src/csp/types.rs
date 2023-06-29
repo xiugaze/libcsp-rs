eac808c2b107cdf3a282e6c8297ecdc4f4a839e7
xiugaze <calebandreano@gmail.com>
2023-06-29 14:51:20 -0500

    Revert CspId Changes

--- src/csp/types.rs
+++ src/csp/types.rs
@@ -8,6 +8,22 @@ use super::{
 };
 
 
+const HEADER_SIZE: u16 = 6;
+const PRIO_MASK: u16        = 6;
+const PRIO_OFFSET: usize    = 6;
+const DST_MASK: u16         = 0x3FFF;
+const DST_OFFSET: usize     = 32;
+const SRC_MASK: u16         = 0x3FFF;
+const SRC_OFFSET: usize     = 18;
+const DPORT_MASK: u8        = 0x3F;
+const DPORT_OFFSET: usize   = 12;
+const SPORT_MASK: u8        = 0x3F;
+const SPORT_OFFSET: usize   = 6;
+const FLAGS_MASK: u8        = 0x3F;
+const FLAGS_OFFSET: usize   = 0;
+
+
+
 #[repr(C)]
 #[derive(Debug, Clone, PartialEq)]
 pub struct CspPacket {
@@ -20,8 +36,13 @@ pub struct CspPacket {
 
 impl CspPacket {
     pub fn new(length: usize, data: [u8; 256], id: CspId) -> Self {
+        let mut header: [u8; 8]= [0; 8];
+        header[2..].copy_from_slice(&data[0..6]);
+
+        let id = CspPacket::strip_id(header);
         CspPacket {
-            length: length as u16, 
+            // length = data size - header size
+            length: length as u16 - HEADER_SIZE, 
             id,
             header: data[0..6].to_owned().try_into().unwrap(),
             data: data[6..length as usize].to_owned(),
@@ -41,22 +62,24 @@ impl CspPacket {
         &self.id
     }
 
-    fn strip_id(id: [u8; 8]) -> CspId {
-        // network order is big endian
-        let id: u64 = Cursor::new(id).read_u64::<BigEndian>();
+    fn strip_id(data: [u8; 8]) -> CspId {
+        // 48 bits (6 bytes of data) to build the ID
+        // Network Byte order is Big-endian, so we need
+        // be64toh (Big Endian 64 unsigned to host architecture)
+        let id = Cursor::new(data).read_u64::<BigEndian>().unwrap();
+        println!("{id:#016X}");
         CspId {
-            priority: todo!(),
-            flags: todo!(),
-            source: todo!(),
-            destination: todo!(),
-            dport: todo!(),
-            sport: todo!(),
+            priority: ((id >> 46) & 0x3) as u8,
+            destination: ((id >> 32) & 0x3FFF) as u16,
+
+        
         }
-    
+        
+        
+
 
     }
 }
-
 impl fmt::Display for CspPacket {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let bytes = &self.data[0..5];
